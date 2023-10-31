9d4bf1ee6c6d99858efd1e2588d33864a1f678c8
brandonom <143345347+brandonom@users.noreply.github.com>
2023-10-31 15:49:25 -0400

    Created questions.js
    created questions array in a seperate folder

---
+++ questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+        title: "question1",
+        choices: [
+            "choice1", "choice2", "choice3", "choice4"
+        ],
+        answer: "choice2"
+    },
+    {
+        title: "question2",
+        choices: [
+            "choice1a", "choice2a", "choice3a", "choice4a"
+        ],
+        answer: "choice2"
+    },
+    {
+        title: "question3",
+        choices: [
+            "choice1b", "choice2b", "choice3b", "choice4b"
+        ],
+        answer: "choice2"
+    },
+    {
+        title: "question4",
+        choices: [
+            "choice1c", "choice2c", "choice3c", "choice4c"
+        ],
+        answer: "choice2"
+    },
+]
